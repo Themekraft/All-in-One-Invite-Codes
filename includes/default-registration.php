e9c9d957ddb2b4625c1a8d782f2ed4d0e7db4ec1
svenl77 <mail@sven-lehnert.de>
2019-02-17 19:56:27 +0000

    Restructure the code

--- includes/default-registration.php
+++ includes/default-registration.php
@@ -31,12 +31,4 @@ function all_in_one_invite_codesregistration_errors( $errors, $sanitized_user_lo
 	return $errors;
 }
 
-add_action( 'user_register', 'all_in_one_invite_codesuser_register' );
-function all_in_one_invite_codesuser_register( $user_id ) {
-	if ( ! empty( $_POST['tk_invite_code'] ) ) {
-		update_user_meta( $user_id, 'tk_all_in_one_invite_code', sanitize_text_field( $_POST['tk_invite_code'] ) );
-
-
-	}
-}
 
--- includes/generate-invite-codes.php
+++ includes/generate-invite-codes.php
@@ -1,1 +1,10 @@
 <?php
+
+add_action( 'user_register', 'all_in_one_invite_codesuser_register' );
+function all_in_one_invite_codesuser_register( $user_id ) {
+	if ( ! empty( $_POST['tk_invite_code'] ) ) {
+		update_user_meta( $user_id, 'tk_all_in_one_invite_code', sanitize_text_field( $_POST['tk_invite_code'] ) );
+
+
+	}
+}
