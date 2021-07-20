9b733f00b1bf38a1778e9a0011c9d3eec34b8e76
Victor Marin <marin250189@gmail.com>
2021-07-19 23:49:03 -0300

    Fix  wp_mail error when destination email is empty
    Fix  wp_mail error when destination email is empty

--- includes/send-invite-email.php
+++ includes/send-invite-email.php
@@ -30,49 +30,55 @@ function all_in_one_invite_codes_send_invite() {
 	$body        = sanitize_textarea_field( esc_html( $_POST['message_text'] ) );
 	$headers     = array( 'Content-Type: text/html; charset=UTF-8' );
 	$invite_code = $invite_code;
+	if ( ! empty( $$to ) ) {
 
-	// Replace Buddy text Shortcodes with form element values
+		// Replace Buddy text Shortcodes with form element values
 
-	// Site Name
-	$site_name = get_bloginfo( 'name' );
-	$subject   = all_in_one_invite_codes_replace_shortcode( $subject, '[site_name]', $site_name );
-    $subject   = all_in_one_invite_codes_replace_shortcode( $subject, '[invite_code]', $invite_code );
-	$body      = all_in_one_invite_codes_replace_shortcode( $body, '[site_name]', $site_name );
-    $body      = all_in_one_invite_codes_replace_shortcode( $body, '[invite_code]', $invite_code );
+		// Site Name
+		$site_name = get_bloginfo( 'name' );
+		$subject   = all_in_one_invite_codes_replace_shortcode( $subject, '[site_name]', $site_name );
+		$subject   = all_in_one_invite_codes_replace_shortcode( $subject, '[invite_code]', $invite_code );
+		$body      = all_in_one_invite_codes_replace_shortcode( $body, '[site_name]', $site_name );
+		$body      = all_in_one_invite_codes_replace_shortcode( $body, '[invite_code]', $invite_code );
 
-	// Invite Link
-    $buddypress_active = false;
-    if(function_exists('bp_is_active')){
-        $buddypress_active = true;
-    }
-    if ($buddypress_active || !all_in_one_invite_codes_is_default_registration() ){
-        $invite_link = '<a href="' . wp_registration_url() . '?invite_code=' . $invite_code . '">Link</a>';
-    }
-	else{
-        $invite_link = '<a href="' . wp_registration_url() . '&invite_code=' . $invite_code . '">Link</a>';
-    }
-	$subject     = all_in_one_invite_codes_replace_shortcode( $subject, '[invite_link]', $invite_link );
-	$body        = all_in_one_invite_codes_replace_shortcode( $body, '[invite_link]', $invite_link );
+		// Invite Link
+		$buddypress_active = false;
+		if(function_exists('bp_is_active')){
+			$buddypress_active = true;
+		}
+		if ($buddypress_active || !all_in_one_invite_codes_is_default_registration() ){
+			$invite_link = '<a href="' . wp_registration_url() . '?invite_code=' . $invite_code . '">Link</a>';
+		}
+		else{
+			$invite_link = '<a href="' . wp_registration_url() . '&invite_code=' . $invite_code . '">Link</a>';
+		}
+		$subject     = all_in_one_invite_codes_replace_shortcode( $subject, '[invite_link]', $invite_link );
+		$body        = all_in_one_invite_codes_replace_shortcode( $body, '[invite_link]', $invite_link );
 
-	// sent the mail
-	$email_param= array("to"=>$to,"subject"=>$subject,"body"=>$body,"headers"=>$headers);
-	$email_param = apply_filters("all_in_one_invite_code_custom_email",$email_param);
-	$send = wp_mail( $email_param["to"], $email_param["subject"] ,$email_param["body"]  ,$email_param["headers"]  );
+		// sent the mail
+		$email_param= array("to"=>$to,"subject"=>$subject,"body"=>$body,"headers"=>$headers);
+		$email_param = apply_filters("all_in_one_invite_code_custom_email",$email_param);
 
-	$all_in_one_invite_codes_options = get_post_meta( $post_id, 'all_in_one_invite_codes_options', true );
+		$send = wp_mail( $email_param["to"], $email_param["subject"] ,$email_param["body"]  ,$email_param["headers"]  );
 
-	// Assign the mail tro the code to make sure this code can only get used from the invited user.
-	if ( empty( $all_in_one_invite_codes_options['email'] ) ) {
-		$all_in_one_invite_codes_options['email'] = $to;
-		update_post_meta( $post_id, 'all_in_one_invite_codes_options', $all_in_one_invite_codes_options );
+		$all_in_one_invite_codes_options = get_post_meta( $post_id, 'all_in_one_invite_codes_options', true );
+
+		// Assign the mail tro the code to make sure this code can only get used from the invited user.
+		if ( empty( $all_in_one_invite_codes_options['email'] ) ) {
+			$all_in_one_invite_codes_options['email'] = $to;
+			update_post_meta( $post_id, 'all_in_one_invite_codes_options', $all_in_one_invite_codes_options );
+		}
+
+		// IF something went wrong during the sent message process
+		if ( ! $send ) {
+			$json['error'] = __( 'Invite could not get send. Please contact the Support.', 'all-in-one-invite-code' );
+			echo json_encode( $json );
+			die();
+		}
+
 	}
 
-	// IF something went wrong during the sent message process
-	if ( ! $send ) {
-		$json['error'] = __( 'Invite could not get send. Please contact the Support.', 'all-in-one-invite-code' );
-		echo json_encode( $json );
-		die();
-	}
+	
 
 	// Great, all done message got sent
 	$json['message'] = __( 'Invite send out successfully', 'buddyforms' );;
